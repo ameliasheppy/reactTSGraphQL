200db0defabeae3396a0bd21bd3689ac9a1a8368
Amy Stanley <98853049+ameliasheppy@users.noreply.github.com>
2023-06-02 16:24:49 -0400

    removed a file that I dont need anymore

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 const {ApolloServer} = require('apollo-server');
 const gql = require('graphql-tag')
 const mongoose = require('mongoose')
-const { MONGODB } = require('./config.js')
-//mongoose is our ORM!
-//now lets write our gql types
 
-const typeDefs = gql`
-    type Query{
-        sayHi: String
-    }`
-//need resolvers that will have a resolver called sayHi. 
-//it will process what the query returns and do something with it
-//put all of the queries in Query obj and mutations in a Mutation obj
-const resolvers = {
-    Query:{
-        sayHi: () => 'Hello World'
-    }
-}
+//mongoose is our ORM!
+//no
+//handy to keep relative imports together and then other imports together
+const typeDefs = require('./graphql/typeDefs')
+const { MONGODB } = require('./config.js')
+const Post = require('./models/Post')
 
 //pass in our gql's in here! the server needs them
 const server = new ApolloServer({
